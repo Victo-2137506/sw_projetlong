ed2ceaea13f426aeaa305e24b4c6b846f8bae09d
Victo-2137506 <2137506@carrefour.cegepvicto.ca>
2025-05-09 09:48:21 -0400

    Ajout d/un middleware

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import express from 'express'
-import sql from './src/config/db_pg'
+import express from 'express';
+import morgan from 'morgan';
+import sql from './src/config/db_pg.js';
 import tacheRoutes from './src/routes/tache.route.js';
+import authentification from './src/middlewares/authentification.middleware.js';
 
 // Créer une application express
 const app = express();
 
-app.use('/api/taches', tacheRoutes)
+app.use('/api/taches', authentification, tacheRoutes)
+app.use(authentification)
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 3000;
--- src/routes/tache.route.js
+++ src/routes/tache.route.js
@@ -2,26 +2,11 @@ import express from 'express';
 const router = express.Router();
 
 import { AfficherTachesUsager } from '../controllers/tache.controller.js'
+import authentification from '../middlewares/authentification.middleware.js';
 
 // Route pour trouver toutes les taches de l'usager
-router.get('/tache', AfficherTachesUsager);
-// Route pour afficher le detail d'une tache
-router.get('/:id');
-// Route pour ajouter, modifier, modifier le statue et supprimer une tache
-router.post('/');
-router.put('/')
-router.put('/')
-router.delete('/')
-// Route pour ajouter, modifier, modifier le statue et supprimer une sous-tache
-router.post('/');
-router.put('/')
-router.put('/')
-router.delete('/')
-// Route pour ajouter un utilisateur
-router.post('/')
-// Route pour recuperer la cle api ou en redemander une nouvelle
-router.get('/')
-router.get('/')
+router.get('/tache', authentification, AfficherTachesUsager);
+
 
 
 export default router;
