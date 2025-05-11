fa80ef0ca2b7b672fc53a9877b90f1d6eda3b473
Victo-2137506 <2137506@carrefour.cegepvicto.ca>
2025-05-11 13:08:27 -0400

    p

--- src/routes/tache.route.js
+++ src/routes/tache.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-import { AfficherTachesUsager, AjouterUtilisateur, recupererCleApiUtilisateur } from '../controllers/tache.controller.js'
+import { AfficherTachesUsager, AjouterUtilisateur, Demandercle } from '../controllers/tache.controller.js'
 import authentification from '../middlewares/authentification.middleware.js';
 
 // Route pour trouver toutes les taches de l'usager
@@ -9,7 +9,7 @@ router.get('/tache', authentification, AfficherTachesUsager);
 
 router.post('/utilisateur', AjouterUtilisateur)
 
-router.post('/cle-api', recupererCleApiUtilisateur);
+router.post('/cle-api', Demandercle);
 
 
 export default router;
