da9b0ffdaf5894726d2e0dec604283a7a3f8f37a
Victo-2137506 <2137506@carrefour.cegepvicto.ca>
2025-05-13 13:21:40 -0400

    retoure du d

--- src/models/tache.model.js
+++ src/models/tache.model.js
@@ -23,7 +23,7 @@ function ValidationCle(cleApi) {
 
 // Code fait par ChatGPT pour generer une clé d'api
 function genererCleAPI() {
-    const cle = crypto.randomBytes(15).toString("hex");
+    return cle = crypto.randomBytes(15).toString("hex");
 }
 
 function afficherToutesTaches(utilisateurId, toutes) {
